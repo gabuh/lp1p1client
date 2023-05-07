0494458a84bf67de47ae01e86a7648aba9b30d98
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-05-07 16:16:58 -0300

    first commit

---
+++ src/main/java/br/edu/ifsp/lp1p1client/Lp1p1clientApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.lp1p1client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp1p1clientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp1p1clientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/lp1p1client/Lp1p1clientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.lp1p1client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp1p1clientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
